cf9f5135cdf1ace3dfd0f201746981448a8b79b9
Minh Nguyen <minh.nguyen@ocit.ocgov.com>
2024-10-21 09:34:16 -0700

    Added edit and delete snippet

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/db";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import * as actions from "@/actions";
 
 interface SnippetShowPageProps {
   params: {
@@ -17,13 +18,18 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     return notFound();
   }
 
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
+
   return (
     <div>
       <div className="flex m-4 justify-between items-center">
         <h1 className="text-xl font-bold">{snippet.title}</h1>
         <div className="flex gap-4">
           <Link href={`${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
-          <button className="p-2 border rounded">Delete</button>
+          <form action={deleteSnippetAction}>
+            <button type="submit" className="p-2 border rounded">Delete</button>
+          </form>
+          
         </div>
       </div>
       <pre className="p-3 border rounded bg-gray-200 border-gray-200">
